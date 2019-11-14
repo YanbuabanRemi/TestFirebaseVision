ac001801081a579a5f73658696f3714f5a0df68d
paris <paris@dsquad.fr>
2019-11-14 10:57:39 +0100

    Initial commit

---
+++ app/src/main/java/com/example/testfirebasevision/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.testfirebasevision
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    //private  fun imageFromPath(Url : string) {
+        // [START image_from_path]
+        //var image: FirebaseVisionImage
+        //try {
+         //   image = FirebaseVisionImage.fromFilePath(
+        //        MainMenuActivity,
+        //        Uri.fromFile(new File(Url)))
+        //} catch (e: IOException) {
+        //    e.printStackTrace()
+        //}
+        // [END image_from_path]
+    //}
+}
